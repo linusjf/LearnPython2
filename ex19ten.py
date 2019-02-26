1b1e5f53341210600f1d09be9921ed290e15b105
fernal73 <linus.fernandes@gmail.com>
2019-02-27 04:26:17 +0530

    Exercise 19 added.

---
+++ ex19ten.py
@@ -0,0 +1,25 @@
+def add(first, second):
+	print  first + second
+
+print "We can just give the function numbers directly:"
+add(20, 30)
+
+print "OR, we can use variables from our script:"
+one = 10
+two = 50
+add(one, two)
+
+print "We can even do math inside too:"
+add(10 + 20, 5 + 6)
+
+print "And we can combine the two, variables and math:"
+add(one + 100,two + 1000)
+
+add(one,100)
+add(45,two)
+add(one+100,two)
+add(one,two+45)
+add(one-two,two-one)
+add(one*45,two*45)
+
+
