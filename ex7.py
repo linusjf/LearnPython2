aec58f623b32e93380e5320e0d12f39ed7d6138d
fernal73 <linus.fernandes@gmail.com>
2019-02-26 07:37:18 +0530

    Updated ex7.py. Added ex8.py.

--- ex7.py
+++ ex7.py
@@ -1,8 +1,14 @@
 print "Mary had a little lamb."
+# This is plain showing off. Why  format a 
+# literal? 
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
+# So this displays the period ten 
+# times...Hmm..
 print "." * 10  # what'd that do?
 
+# prints out CheeseBurger ,a character at 
+# a time
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -18,5 +24,7 @@ end12 = "r"
 
 # watch that comma at the end.  try 
 # removing it to see what happens
+# The comma keeps it all in one line. 
+# Works like a concatenator.
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
