3e657f9d02244b052889eba9079696f73f551b68
fernal73 <linus.fernandes@gmail.com>
2019-02-26 13:39:36 +0530

    Modified ex13.py for raw_input.

--- ex13.py
+++ ex13.py
@@ -7,3 +7,10 @@ print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
 
+print "Collecting more input: "
+print "fourth :",
+fourth = raw_input()
+print "fifth: ",
+fifth = raw_input()
+print  "Your fourth variable is: ", fourth
+print "Your fifth variable is: ", fifth
