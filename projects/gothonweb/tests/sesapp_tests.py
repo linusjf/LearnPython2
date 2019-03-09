be9f54d4b0a859db8f3f0e70ca46a8902f26d3f8
fernal73 <linus.fernandes@gmail.com>
2019-03-09 18:45:39 +0530

    updated unit tests for sesapp

--- projects/gothonweb/tests/sesapp_tests.py
+++ projects/gothonweb/tests/sesapp_tests.py
@@ -35,8 +35,6 @@ def invokeTheBridge():
 
 def invokeEscapePod():
     resp = invokeTheBridge()
-    print 'invokeEscapePod : ',
-    print resp
     return invokePostGame(
             data={'action':'slowly place the bomb'})
 
@@ -52,43 +50,88 @@ def testSessionID():
 
 def testShoot():
     resp1 = invokeGetGame('/game')
-    assertResponse(resp1, status=200, contains='Central Corridor')
-    resp1 = invokePostGame(data={'action':'shoot!'})
-    assertResponse(resp1, status=200, contains=map.generic_death.name)
+    assertResponse(resp1, status=200,
+            contains='Central Corridor')
+    resp1 = invokePostGame(
+            data={'action':'shoot!'})
+    assertResponse(resp1,
+            status=200,
+            contains=map.generic_death.name)
 
 def testDodge():
     resp1 = invokeGetGame()
-    assertResponse(resp1, status=200, contains='Central Corridor')
+    assertResponse(resp1,
+            status=200,
+            contains='Central Corridor')
     resp1 = invokePostGame(data={'action':'dodge!'})
-    assertResponse(resp1, status=200, contains=map.generic_death.name)
+    assertResponse(resp1,
+            status=200,
+            contains=map.generic_death.name)
 
 def testTellAJoke():
     resp1 = invokeGetGame()
-    assertResponse(resp1, status=200, contains='Central Corridor')
-    resp1 = invokePostGame(data={'action':'tell a joke'})
-    assertResponse(resp1, status=200, contains=map.laser_weapon_armory.name)
+    assertResponse(resp1,
+            status=200,
+            contains='Central Corridor')
+    resp1 = invokePostGame(
+            data={'action':'tell a joke'})
+    assertResponse(resp1,
+            status=200,
+            contains=map.laser_weapon_armory.name)
+
+def testCentralCorridorError():
+    resp1 = invokeGetGame()
+    assertResponse(resp1,
+            status=200,
+            contains='Central Corridor')
+    resp1 = invokePostGame(
+            data={'action':'*'})
+    assertResponse(resp1,
+            status=200,
+            contains='The End')
 
 def testInputCode():
     resp = invokeLaserWeaponArmory()
     resp1 = invokePostGame(data={'action':'0132'})
-    assertResponse(resp1, status=200, contains=map.the_bridge.name)
+    assertResponse(resp1,
+            status=200,
+            contains=map.the_bridge.name)
+
+def testInputCodeError():
+    resp = invokeLaserWeaponArmory()
+    resp1 = invokePostGame(data={'action':'*'})
+    assertResponse(resp1,
+            status=200,
+            contains=map.generic_death.name)
+
 
 def testTheBridge():
     resp = invokeTheBridge()
-    resp = invokePostGame(data={'action':'slowly place the bomb'})
-    assertResponse(resp,status=200,contains=map.escape_pod.name)
+    resp = invokePostGame(
+            data={'action':'slowly place the bomb'})
+    assertResponse(resp,
+            status=200,
+            contains=map.escape_pod.name)
+
+def testTheBridgeError():
+    resp = invokeTheBridge()
+    resp = invokePostGame(data=
+            {'action':'throw the bomb'})
+    assertResponse(resp,
+            status=200,
+            contains=map.generic_death.name)
+
 
 def testEscapePod():
     resp = invokeEscapePod()
     resp = invokePostGame(data={'action':'2'})
-    assertResponse(resp,status=200,contains=map.the_end_winner.name)
+    assertResponse(resp,
+            status=200,
+            contains=map.the_end_winner.name)
 
 def testEscapePodError():
     resp = invokeEscapePod()                    
     resp = invokePostGame(data={'action':'5'})
-    print 'url : %r' % resp.url
-    print 'testEscapePodError: ',
-    print resp
     assertResponse(resp,status=200,
             contains=map.the_end_loser.name)   
 
